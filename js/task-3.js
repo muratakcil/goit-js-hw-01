67d3f045078d5aa280853ee619a6d8361402aa60
muratakcil <muratakcil53@gmail.com>
2025-10-03 10:51:30 +0300

    goit-js-hw-01

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth (content,padding,border){
+    const totalWidth = content + padding*2 + border*2;
+    return totalWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
