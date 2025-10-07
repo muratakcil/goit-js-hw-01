1081be8039366add1598c6049e3c1cf92ba74c97
muratakcil <muratakcil53@gmail.com>
2025-10-07 13:27:27 +0300

    jvscript

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
-function getElementWidth (content,padding,border){
-    const totalWidth = content + padding*2 + border*2;
+function getElementWidth (content, padding, border){
+    const contentNumeric = parseFloat(content); 
+    const paddingNumeric = parseFloat(padding); 
+    const borderNumeric  = parseFloat(border);  
+
+    const totalWidth = contentNumeric + paddingNumeric * 2 + borderNumeric * 2;
+    
     return totalWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
